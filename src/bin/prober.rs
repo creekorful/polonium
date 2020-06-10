e8a8a50fe952dfd1bc40f2afb7f4c56ad55666d9
Aloïs Micard <alois@micard.lu>
2020-06-10 13:51:12 +0200

    Basic prober implementation

--- src/bin/prober.rs
+++ src/bin/prober.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::io::{Read, Write};
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let target = args.get(1).expect("target is required");
+
+    let mut stream = TcpStream::connect(target)?;
+    stream.write("HEAD / HTTP/1.1\n\n".as_ref())?;
+
+    let mut buffer = String::new();
+    stream.read_to_string(&mut buffer)?;
+    print!("{}", buffer);
+
+    Ok(())
 }

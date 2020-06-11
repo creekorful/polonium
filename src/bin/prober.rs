838f4cc6a1ce3471adc965fd9a44041c16514676
Aloïs Micard <alois@micard.lu>
2020-06-11 09:42:13 +0200

    Create grab_banner function

--- src/bin/prober.rs
+++ src/bin/prober.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::{env, io};
 use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str;
@@ -9,16 +9,22 @@ fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let target = args.get(1).expect("target is required");
 
+    let banner = grab_banner(target)?;
+    print!("{}", banner);
+
+    Ok(())
+}
+
+fn grab_banner(svc: &str) -> io::Result<String> {
     // TODO: use connect_timeout instead
-    let mut stream = TcpStream::connect(target).expect("unable to connect to target");
+    let mut stream = TcpStream::connect(svc).expect("unable to connect to target");
     stream.set_read_timeout(Option::from(Duration::from_millis(500))).expect("unable to set_read_timeout");
 
     let mut buffer = [0; 512];
 
     // Try to read banner right after connecting
     if stream.read(&mut buffer).is_ok() {
-        print!("{}", str::from_utf8(&buffer).unwrap());
-        return Ok(());
+        return Ok(String::from(str::from_utf8(&buffer).unwrap()));
     }
 
     // If nothing was returned, send a dummy request
@@ -26,7 +32,5 @@ fn main() -> std::io::Result<()> {
 
     // Try to read again
     stream.read(&mut buffer)?;
-    print!("{}", str::from_utf8(&buffer).unwrap());
-
-    Ok(())
+    return Ok(String::from(str::from_utf8(&buffer).unwrap()));
 }

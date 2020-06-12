05c18eec81440ac37894e6e4981ede13fff3564d
Aloïs Micard <alois@micard.lu>
2020-06-12 08:32:59 +0200

    Allow customization of connect/read timeout

--- src/bin/prober.rs
+++ src/bin/prober.rs
@@ -11,13 +11,31 @@ fn main() -> std::io::Result<()> {
         .version("0.0.1")
         .author("Aloïs Micard <alois@micard.lu>")
         .about("Probe given address to gather banner details")
-        .arg(Arg::with_name("address").required(true).help("address of the service to be scanned"))
+        .arg(Arg::with_name("address")
+            .required(true)
+            .value_name("ADDRESS:PORT")
+            .help("address of the service to be scanned"))
+        .arg(Arg::with_name("connect-timeout")
+            .long("connect-timeout")
+            .value_name("VALUE")
+            .default_value("1000")
+            .help("connect timeout (ms)"))
+        .arg(Arg::with_name("read-timeout")
+            .long("read-timeout")
+            .value_name("VALUE")
+            .default_value("500")
+            .help("read timeout (ms)"))
         .get_matches();
 
     let target = matches.value_of("address").unwrap();
 
-    let connect_timeout = Duration::from_millis(1000);
-    let read_timeout = Duration::from_millis(500);
+    let connect_timeout = matches.value_of("connect-timeout").unwrap();
+    let connect_timeout = connect_timeout.parse::<u64>().unwrap();
+    let connect_timeout = Duration::from_millis(connect_timeout);
+
+    let read_timeout = matches.value_of("read-timeout").unwrap();
+    let read_timeout = read_timeout.parse::<u64>().unwrap();
+    let read_timeout = Duration::from_millis(read_timeout);
 
 
     let banner = grab_banner(target, connect_timeout, read_timeout).expect("unable to grab banner");

aff15547d966b38f77352ba3cde41978db732021
Aloïs Micard <alois@micard.lu>
2020-06-12 10:16:59 +0200

    Separate into lib.rs

--- src/bin/prober.rs
+++ src/bin/prober.rs
@@ -1,7 +1,3 @@
-use std::io::{ErrorKind, Read, Write};
-use std::net::{SocketAddr, TcpStream};
-use std::str;
-use std::str::FromStr;
 use std::time::Duration;
 
 use clap::{App, Arg};
@@ -47,40 +43,9 @@ fn main() -> std::io::Result<()> {
     let write_timeout = Duration::from_millis(write_timeout);
 
 
-    let banner = grab_banner(target, connect_timeout, read_timeout, write_timeout)
+    let banner = boron::probing::grab_banner(target, connect_timeout, read_timeout, write_timeout)
         .expect("unable to grab banner");
     print!("{}", banner);
 
     Ok(())
-}
-
-fn grab_banner(address: &str, connect_timeout: Duration,
-               read_timeout: Duration, write_timeout: Duration) -> Result<String, failure::Error> {
-    let address = SocketAddr::from_str(address)?;
-
-    let mut stream = TcpStream::connect_timeout(&address, connect_timeout)?;
-    stream.set_read_timeout(Option::from(read_timeout))?;
-    stream.set_write_timeout(Option::from(write_timeout))?;
-
-    let mut buffer = [0; 512];
-
-    // Try to read banner right after connecting
-    let result = stream.read(&mut buffer);
-    if result.is_ok() {
-        return Ok(String::from(str::from_utf8(&buffer)?));
-    }
-
-    // If timeout related error happens, do not fails
-    // because we may need to talk first
-    let error = result.err().unwrap();
-    if error.kind() != ErrorKind::WouldBlock {
-        return Err(failure::err_msg(error));
-    }
-
-    // If nothing was returned, send a dummy request
-    stream.write("HEAD / HTTP/1.1\n\n".as_ref())?;
-
-    // Try to read again
-    stream.read(&mut buffer)?;
-    return Ok(String::from(str::from_utf8(&buffer)?));
 }
---
+++ src/lib.rs
@@ -0,0 +1,38 @@
+pub mod probing {
+    use std::io::{ErrorKind, Read, Write};
+    use std::net::{SocketAddr, TcpStream};
+    use std::str;
+    use std::str::FromStr;
+    use std::time::Duration;
+
+    pub fn grab_banner(address: &str, connect_timeout: Duration,
+                       read_timeout: Duration, write_timeout: Duration) -> Result<String, failure::Error> {
+        let address = SocketAddr::from_str(address)?;
+
+        let mut stream = TcpStream::connect_timeout(&address, connect_timeout)?;
+        stream.set_read_timeout(Option::from(read_timeout))?;
+        stream.set_write_timeout(Option::from(write_timeout))?;
+
+        let mut buffer = [0; 512];
+
+        // Try to read banner right after connecting
+        let result = stream.read(&mut buffer);
+        if result.is_ok() {
+            return Ok(String::from(str::from_utf8(&buffer)?));
+        }
+
+        // If timeout related error happens, do not fails
+        // because we may need to talk first
+        let error = result.err().unwrap();
+        if error.kind() != ErrorKind::WouldBlock {
+            return Err(failure::err_msg(error));
+        }
+
+        // If nothing was returned, send a dummy request
+        stream.write("HEAD / HTTP/1.1\n\n".as_ref())?;
+
+        // Try to read again
+        stream.read(&mut buffer)?;
+        return Ok(String::from(str::from_utf8(&buffer)?));
+    }
+}
